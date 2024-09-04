efb0192db7d4690f0a2e6798364266d22f6b6878
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-04 16:54:32 +0800

    Create Day3.java
    Hari ke 3 program 100 days of coding

---
+++ Day3.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama saya Erick surya pratama");
+        System.out.println("Umur 18 tahun");
+        System.out.println("Alamat di jalan poros majene");
+    }
+
+}
