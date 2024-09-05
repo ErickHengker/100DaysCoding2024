4ea5fe5b16d9a897faed88fc5594e1384bcc6b2b
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-05 09:49:51 +0800

    Create Day4.java
    Hari ke 4 program 100 days of coding

---
+++ Day4.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        System.out.print("Sup homie");
+        System.out.print("Rizzu here");
+        System.out.print("Yoroshiku");
+
+        System.out.printf("Nama saya %s dan umur saya %d tahun.", "Rizzu", 18);
+
+    }
+
+}
