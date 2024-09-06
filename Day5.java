2413a28d36d2488c42917bd6dee2589cd35b500d
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-06 10:21:00 +0800

    Day5.java
    Hari ke 5 program 100 days of coding

---
+++ Day5.java
@@ -0,0 +1,19 @@
+
+package printt;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        System.out.println("Nama saya erick");
+        //ini adalah output nama 
+        System.out.print("Umur 18");
+        //ini adalah output umur 
+        System.out.printf("Nama saya %s umur saya %d tahun.","Rizzu", 18);
+        /**dan ini adalah contoh output printf yang memungkinkan penggunaan
+         *placeholder (%d,%s,%f,dll) untuk menyisipkan nilai variabel ke dalam
+         * string yang dicetak
+         */
+        
+    }
+    
+}
