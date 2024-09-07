2345c0fbd36f6de697cb8c4c37b3fd7b4554d02e
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-07 18:23:33 +0800

    Day6.java
    Hari ke 6 program 100 days of coding

---
+++ Day6.java
@@ -0,0 +1,12 @@
+package day6;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        String nama;
+        String Nim;
+        String alamat;
+        //pembuatan variabel deklarasi
+    }
+
+}
