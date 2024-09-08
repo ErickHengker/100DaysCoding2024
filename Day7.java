041f26cf2e38928111cdf25f0f634a6a709c72e8
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-08 21:43:12 +0800

    Create Day7.java
    Hari ke 7 program 100 days of coding

---
+++ Day7.java
@@ -0,0 +1,17 @@
+package day7;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        //inisialisasi 
+        String nama = "Erick surya pratama";
+        String umur = "18 tahun";
+        String alamat = "jalan poros majene-mamuju";
+
+        //menampilkan hasil
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Alamat: " + alamat);
+    }
+
+}
