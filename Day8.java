6ea2d561f9063358ab8212d6f6021ee3dae4c8be
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-09 20:31:51 +0800

    Create Day8.java
    Hari ke 8 program 100 days of coding

---
+++ Day8.java
@@ -0,0 +1,21 @@
+package day8;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        //deklarasi dan inisialisasi variabel
+        int umur = 20;
+        System.out.println("umur salah: " + umur);
+
+        //update nilai variabel
+        umur = 18;
+        System.out.println("umur yang benar: " + umur);
+
+        String nama = "erick sura paratma";
+        System.out.println("Nama salah/typo : " + nama);
+
+        nama = "Erick Surya Pratama";
+        System.out.println("Nama benar : " + nama);
+    }
+
+}
