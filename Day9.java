f1c95fd132e3a32e72eb83feb8a00cca490a7791
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-10 23:09:57 +0800

    Create Day9.java
    Hari ke 9 program 100 days of coding

---
+++ Day9.java
@@ -0,0 +1,17 @@
+package day9;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        //contoh variabel constanta
+        final int x = 27;
+        x = 100;
+        //jika di run ini akan error
+
+        //contoh lain
+        final double TglWbln = 27.05;
+        TglWbln = 27.11
+        System.out.println("Tanggal dan bulan lahir" + TglWbln);
+    }
+
+}
