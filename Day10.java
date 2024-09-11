d22c77b05648b69534a343428eb2809a40e08f24
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-11 22:05:38 +0800

    Create Day10.java
    Hari ke 10 program 100 days of coding

---
+++ Day10.java
@@ -0,0 +1,25 @@
+package day10;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        byte a = 27;
+        //nilai positif
+        //nilai negatif
+        byte c = 127;
+        byte b = -14;
+        //nilai maksimal
+        byte d = -128;
+        //nilai minimal
+
+        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai b: " + b);
+        System.out.println("Nilai c: " + c);
+        System.out.println("Nilai d: " + d);
+
+        byte e = (byte) (a + b);
+        System.out.println("Hasil penjumlahan a dan b: " + e);
+        //penjumlahan a dan b
+    }
+
+}
