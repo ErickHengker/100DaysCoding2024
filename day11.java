bfe472fcd96231c43648bd7008f26e3b9b2e7f75
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-12 21:00:17 +0800

    Create day11.java
    hari ke 11 100 days of coding

---
+++ day11.java
@@ -0,0 +1,13 @@
+package day11_;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        short nilai = 100;
+        short umur = 18;
+        System.out.println("Saya dapat nilai " + nilai);
+        System.out.println("Umur saya " + umur);
+
+    }
+
+}
