fa41e8b5fc3bd80deac8bd78c8c2dcd6f5c45612
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-13 21:36:55 +0800

    Create day12.java
    hari ke 12 program 100 days of coding

---
+++ day12.java
@@ -0,0 +1,10 @@
+package day12;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        int jumlah = 100;
+        System.out.println("Jumlah populasi :" + jumlah);
+    }
+
+}
