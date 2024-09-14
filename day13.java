f9a6aa143e77553b04a88d5cacc1a307d9f63f42
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-14 16:36:16 +0800

    Create day13.java
    hari ke 13 program 100 days of coding

---
+++ day13.java
@@ -0,0 +1,38 @@
+package evaluasi.pkg1;
+
+public class Evaluasi1 {
+
+    public static void main(String[] args) {
+        byte a = 27;
+        //nilai positif
+        byte b = -27;
+        //nilai negatif
+        byte c = 127;
+        //nilai maksimal
+        byte d = -128;
+        //nilai minimal
+
+        System.out.println("A" + a);
+        System.out.println("B" + b);
+        System.out.println("C" + c);
+        System.out.println("D" + d);
+
+        short total = 3200;
+        int TanggalLahir = 27;
+        System.out.println("Total uang" + total);
+        System.out.println("Tanggal lahir" + TanggalLahir);
+
+        String day = "day";
+        int angka = 13;
+
+        //Buatlah 2 variabel untuk, untuk variabel pertama berisi nilai string "day"
+        //dan variabel kedua berisi nilai int 13, panggil ke dua nilai variabel
+        //menggunakan print untuk mencetak 1 baris
+        String hari = "day";
+        int tgl = 13;
+        System.out.println("" + hari
+                + " " + tgl);
+
+    }
+
+}
