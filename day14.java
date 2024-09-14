471184d11fea16ff1650ce6c23e84811df27e31f
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-14 23:41:26 +0800

    Create day14.java
    hari ke 14 program 100 days of coding

---
+++ day14.java
@@ -0,0 +1,13 @@
+package day14;
+
+public class Day14 {
+
+    public static void main(String[] args) {
+        long angka = 12345678910L;
+        System.out.println("Angka 1-10 " + angka);
+        //penting untuk menambahkan huruf L atau angka 1 diakhir nilai lateral
+        //tapi lebih disarankan memakai huruf L di akhir jika ingin menandai
+        // bahwa bilangan tersebut bertipe long
+
+    }
+}
