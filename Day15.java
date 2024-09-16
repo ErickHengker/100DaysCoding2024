40dd7388e372e594650c7c018974961bdb63237a
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-16 23:50:06 +0800

    Create Day15.java
    hari ke 15 program 100 days of coding

---
+++ Day15.java
@@ -0,0 +1,10 @@
+package day15;
+
+public class Day15 {
+
+    public static void main(String[] args) {
+        double nilaiDesimal = 10.123456789;
+        System.out.println("Nilai nya " + nilaiDesimal);
+    }
+
+}
