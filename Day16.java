79e19d38c53b8315bd73ee43f4eda211f7dfabd5
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-18 00:03:55 +0800

    Create Day16.java
    hari ke 16 prograkm 100 days of coding

---
+++ Day16.java
@@ -0,0 +1,11 @@
+
+package day16;
+
+public class Day16 {
+
+    public static void main(String[] args) {
+        float num1 = 3.14f; 
+        System.out.println("num1 : " + num1);
+    }
+    
+}
