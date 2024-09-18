86b746cb43e4014204b31df738ab03f908bbe554
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-18 21:12:35 +0800

    Create Day17.java
    Hari ke 17 program 100 days of coding

---
+++ Day17.java
@@ -0,0 +1,19 @@
+package day17;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        //tipe char
+        char nm1 = 'E';
+        char nm2 = 'r';
+        char nm3 = 'i';
+        char nm4 = 'c';
+        char nm5 = 'k';
+        System.out.print("Nama saya :" + nm1);
+        System.out.print("" + nm2);
+        System.out.print("" + nm3);
+        System.out.print("" + nm4);
+        System.out.print("" + nm5);
+    }
+
+}
