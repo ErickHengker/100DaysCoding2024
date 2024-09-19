0dc243068860d37b886f9ffd0c3384c1f974a84c
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-19 22:03:27 +0800

    Create Day18.java
    hari ke 18 program 100 days of coding

---
+++ Day18.java
@@ -0,0 +1,13 @@
+package day18;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println("is human breath? " + benar);
+        System.out.println("is fish breath? " + salah);
+    }
+
+}
