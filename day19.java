7cad1802cd5b88f7991875c00b02198c9f9e888c
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-20 21:35:38 +0800

    Create day19.java

---
+++ day19.java
@@ -0,0 +1,12 @@
+package javaapplication27;
+
+public class JavaApplication27 {
+
+    public static void main(String[] args) {
+        String nama = "erick";
+        String salam = "Halo, " + nama;
+        System.out.println(salam);
+
+    }
+
+}
