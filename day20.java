4afd14b7a9d32cbea617bcdba368460247175ae6
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-21 07:49:41 +0800

    Create day20.java

---
+++ day20.java
@@ -0,0 +1,10 @@
+package day20;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        byte angka = 27;
+        System.out.println("Tanggal lahir : " + angka);
+    }
+
+}
