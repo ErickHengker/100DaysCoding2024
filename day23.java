4e0486c88b0a38f72e79860e697d2e4cf628f092
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-24 22:40:48 +0800

    Create day23.java
    hari ke 23

---
+++ day23.java
@@ -0,0 +1,10 @@
+package day23;
+
+public class Day23 {
+
+    public static void main(String[] args) {
+        long jumlah = 275000000;
+        System.out.println("Jumlah populasi di indonesia " + jumlah);
+    }
+
+}
