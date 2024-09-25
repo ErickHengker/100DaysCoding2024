4bdfafdedf2fd1f0aa0573e9cb487b0c239ae261
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-25 23:15:29 +0800

    Create day24.java

---
+++ day24.java
@@ -0,0 +1,10 @@
+package day24;
+
+public class Day24 {
+
+    public static void main(String[] args) {
+        float tinggi = 167.05f;
+        System.out.println("Tinggi saya : " + tinggi);
+    }
+
+}
