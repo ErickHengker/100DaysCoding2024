6dd1f7a8b81bb57a04bfcb62eac916caf547e6e7
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-26 23:15:51 +0800

    Create day25.java

---
+++ day25.java
@@ -0,0 +1,11 @@
+package day25;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+        double tinggi = 167.00;
+        System.out.println("Tinggi saya : " + tinggi);
+
+    }
+
+}
