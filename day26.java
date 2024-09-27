0f62adbd2d04640e1bb5eab1dab3ef2b0497c994
ErickHengker <nholdyrealscout10133@gmail.com>
2024-09-27 21:10:59 +0800

    Create day26.java
    hari ke 26

---
+++ day26.java
@@ -0,0 +1,11 @@
+package day26;
+
+public class Day26 {
+
+    public static void main(String[] args) {
+        Character objek = 'E';
+        System.out.println("inisial :" + objek);
+
+    }
+
+}
