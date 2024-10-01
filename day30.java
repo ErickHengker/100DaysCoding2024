ad94c83d805ef053f96d977dd6e216945c3ea6f7
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-01 23:26:43 +0800

    Create day30.java

---
+++ day30.java
@@ -0,0 +1,12 @@
+package day30;
+
+import java.math.BigDecimal;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+        BigDecimal angka = new BigDecimal("123456789");
+        System.out.println("Nilai dari big decimal " + angka);
+    }
+
+}
