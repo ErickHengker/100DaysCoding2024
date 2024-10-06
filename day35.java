ef909cef52687c3a37e1ba23dbc99a0050635ca3
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-06 23:15:27 +0800

    Create day35.java

---
+++ day35.java
@@ -0,0 +1,25 @@
+package ErickSuryaPratama;
+
+public class Main {
+    
+    public static void main(String[] args) {
+    
+        int angka = 27;
+        int angka1 = 28;
+        int angka2 = angka + angka1;
+        //penjumlahan
+        System.out.println("Hasil" +angka2); //cara1
+        System.out.println(angka + angka1); //cara2
+        
+        //pengurangan
+        int angka3 = angka - angka1;
+        System.out.println("Hasil" +angka3);//cara1
+        System.out.println(angka - angka1);
+        
+        
+        
+        
+
+    }
+    
+}
