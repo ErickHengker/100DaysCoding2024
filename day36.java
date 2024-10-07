f83890398c6ef1f77fa90560f8f97463cdc71b80
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-07 22:09:39 +0800

    Create day36.java

---
+++ day36.java
@@ -0,0 +1,32 @@
+package ErickSuryaPratama;
+
+public class Main {
+    
+    public static void main(String[] args) {
+    
+        int angka = 27;
+        int angka1 = 28;
+        int angka2 = angka * angka1;
+        //perkalian
+        System.out.println("Hasil"  +angka2); //cara1
+        System.out.println(angka * angka1); //cara2
+        
+        //pembagian
+        int angka3 = angka / angka1;
+        System.out.println("Hasil"  +angka3);//cara1
+        System.out.println(angka / angka1);
+        
+        //modulo
+        int angka4 = 10;
+        int angka5 = 3;
+        int angka6 = angka4 % angka5;
+        System.out.println(angka4 % angka5);
+        
+        
+        
+        
+        
+
+    }
+    
+}
