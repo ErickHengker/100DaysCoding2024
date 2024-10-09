b8652abea87c3cb4e3e7c12352898ad99dfc056d
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-09 23:32:39 +0800

    Create day38.java

---
+++ day38.java
@@ -0,0 +1,23 @@
+
+package javaapplication43;
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+     //operator penugasan
+     
+     //pengisian nilai
+     int nilai = 10;
+        System.out.println("nilai awal : " +nilai);
+     
+     //penambahan
+     nilai *= 3;
+        System.out.println("perkalian : " +nilai);
+        
+     //pengurangan
+     nilai /= 2;
+        System.out.println("pembagian : " +nilai);
+             
+     
+    }
+    
+}
