5a20db508bd4bf3642315b1582c9bf1686f7e3f4
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-11 23:12:30 +0800

    Create day40.java

---
+++ day40.java
@@ -0,0 +1,14 @@
+package ErickSuryaPratama;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 9;
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(b < a);
+        System.out.println(b > a);
+    }
+    
+}
