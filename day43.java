1a5401273ed061710888deac2ef2352e579b4bae
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-14 23:20:52 +0800

    Create day43.java

---
+++ day43.java
@@ -0,0 +1,17 @@
+package ErickSuryaPratama;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+     Scanner masuk = new Scanner(System.in);
+     System.out.print("Masukkan angka :");
+     int angka = masuk.nextInt();
+     System.out.print("Masukkan angka :");
+     int angka1 = masuk.nextInt();
+     
+     System.out.println(angka>=angka1);
+        System.out.println(angka<=angka1);
+    }
+        
+    }
+
