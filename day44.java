c3c37e8f62c2172a9ff85091394befd6a7677d3d
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-15 23:38:21 +0800

    Create day44.java

---
+++ day44.java
@@ -0,0 +1,19 @@
+package ErickSuryaPratama;
+
+public class Main {
+    
+    public static void main(String[] args) {
+    int a = 2;
+     int b = 27; 
+     int c = 20;
+     
+     boolean angka = a > b && b > a;
+     boolean angka1 = a < c && b > c;
+     System.out.println(angka);
+     System.out.println(angka1);
+     
+     
+    }
+        
+    }
+
