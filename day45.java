bab29dfcafc692b392c3a196ccd7c6730358eae9
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-16 23:50:56 +0800

    Create day45.java

---
+++ day45.java
@@ -0,0 +1,21 @@
+package ErickSuryaPratama;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = true; 
+        boolean c = false;
+        boolean d = true;
+        
+        
+        System.out.println(a || b);
+        System.out.println(b || d);
+        System.out.println(a || c);
+        System.out.println(b || c);
+     
+     
+    }
+        
+    }
+
