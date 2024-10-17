e6d6b38d1376a4a51e042430d69ae4225cb69cf2
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-17 23:50:20 +0800

    Create day46.java

---
+++ day46.java
@@ -0,0 +1,18 @@
+package ErickSuryaPratama;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        
+        
+        System.out.println(!a || b);
+        System.out.println(a || !b);
+        
+     
+     
+    }
+        
+    }
+
