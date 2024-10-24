b01d024e0e3c2831a3f35b0046a87e8a9e223d05
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-24 23:17:21 +0800

    Create day53.java

---
+++ day53.java
@@ -0,0 +1,12 @@
+
+package day53;
+
+public class ErickSuryapratama {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
