4d89d87ff6c21466b3677955d6b51ac4a33e8d14
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-25 23:29:13 +0800

    Create day54.java

---
+++ day54.java
@@ -0,0 +1,11 @@
+package ErickSuryaPratama;
+public class Main {
+    
+    public static void main(String[] args) {
+        for (int i = 0 ; i <= 50 ; i += 2){
+            System.out.println("Hasil :" +i);
+        }
+    }
+        
+    }
+    
