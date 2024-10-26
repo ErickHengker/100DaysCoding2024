5976f9b43f012abfafc0d9a804ce43477b3eda22
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-26 21:16:29 +0800

    Create day56.java

---
+++ day56.java
@@ -0,0 +1,12 @@
+
+package day56;
+
+public class Day56 {
+
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Value of i: " +i);
+                    }
+    }
+    
+}
