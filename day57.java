e2fdd03abc06c4df90960a9f6769c78f47ce2d10
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-28 23:15:26 +0800

    Create day57.java

---
+++ day57.java
@@ -0,0 +1,14 @@
+package ErickSuryaPratama;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        while(a <= 10){
+            System.out.println(a++);
+        }
+        }
+    }
+        
+    
+
