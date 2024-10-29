327bc2d5f2023f13a527904ecd4a8b1f9b02dc59
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-29 23:08:55 +0800

    Create day58.java

---
+++ day58.java
@@ -0,0 +1,12 @@
+package pp;
+
+public class Main {
+    
+    public static void main(String[] args) {
+       int a = 10;
+        while(a >= 5){
+            System.out.println("Decremen " + a);
+            a--; }
+    }
+    
+}
