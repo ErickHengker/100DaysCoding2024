93ce47dceb5e1a5dddc0a19ac60226bf7946b316
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-30 23:18:34 +0800

    Create day59.java

---
+++ day59.java
@@ -0,0 +1,13 @@
+package pp;
+
+public class Main {
+    
+    public static void main(String[] args) {
+       int[] angka = {1, 2, 3, 4, 5};
+
+        for (int angka1 : angka) {
+        System.out.println(angka1);
+}
+    }
+    
+}
