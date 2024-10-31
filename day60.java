a201f4cd9604a9a7680f8ee258049d3c4c7891af
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-31 23:47:33 +0800

    Create day60.java

---
+++ day60.java
@@ -0,0 +1,10 @@
+package soal;
+public class soal2 {
+    public static void main(String[] args) {
+         int a = 1;
+        do {
+            System.out.println("Angka ke-"+a);
+            a++;
+        }while(a <= 10);
+    }
+}
