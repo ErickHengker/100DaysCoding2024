161364c4e27770d31764e964172a4e9ba4eab61e
ErickHengker <nholdyrealscout10133@gmail.com>
2024-10-31 23:53:24 +0800

    Update day60.java

--- day60.java
+++ day60.java
@@ -3,7 +3,7 @@ public class soal2 {
     public static void main(String[] args) {
          int a = 1;
         do {
-            System.out.println("Angka ke-"+a);
+            System.out.println(a);
             a++;
         }while(a <= 10);
     }
