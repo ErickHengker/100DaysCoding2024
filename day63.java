4ba70bd78e6a59a3ffa08207a17b1ca6f228890d
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-03 23:05:47 +0800

    Create day63.java

---
+++ day63.java
@@ -0,0 +1,14 @@
+package ISC;
+
+public class ErickSuryaPratama {
+
+    public static void main(String[] args) {
+       for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue; 
+            }
+            System.out.println("Angka: " + i);
+        }
+    }
+
+}
