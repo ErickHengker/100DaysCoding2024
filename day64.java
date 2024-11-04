93c956c8aab9ccb02dbf357a39146ef276960124
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-04 23:24:46 +0800

    Create day64.java

---
+++ day64.java
@@ -0,0 +1,12 @@
+public class Day64 {
+
+    public static void main(String[] args) {
+         for(int ab=0; ab<15; ab++){
+            if(ab==10){
+                break;
+            }
+            System.out.print(ab);
+        }
+    }
+    
+}
