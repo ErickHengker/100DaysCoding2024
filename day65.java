1b82d26bce86b787aac5bec2a797501f8bda13c2
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-05 23:55:07 +0800

    Create day65.java

---
+++ day65.java
@@ -0,0 +1,11 @@
+public class Day64 {
+
+    public static void main(String[] args) {
+           for (int i = 0; i < 5; i++) {  // Loop luar
+            for (int j = 0; j < 3; j++) {  // Loop dalam
+                System.out.println("i: " + i + ", j: " + j);
+            }
+        }
+    }
+    
+}
