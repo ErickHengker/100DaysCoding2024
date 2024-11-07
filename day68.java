9cf9b6a541dac654d052c717fa26f90e72c4cf61
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-07 23:28:31 +0800

    Create day68.java

---
+++ day68.java
@@ -0,0 +1,11 @@
+
+public class day68 {
+     public static void cetakPesan() {
+        System.out.println("Erick Isc member");
+    }
+
+    public static void main(String[] args) {
+        cetakPesan();
+    }
+}
+
