adcc38b9ea6b389c6941f87005c2a786a3effcb0
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-08 23:10:16 +0800

    Create day68.java

---
+++ day68.java
@@ -0,0 +1,13 @@
+package day68;
+
+public class Day68 {
+
+    public static void main(String[] args) {
+        Day68 Rizz = new Day68();
+        System.out.println(Rizz.nama());
+    }
+
+    String nama() {
+        return "Rizz";
+    }
+}
