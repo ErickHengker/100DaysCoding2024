9a57be7bb8309cf28ed7a421eda0b0df741a3a49
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-09 21:07:19 +0800

    Create Day69.java

---
+++ Day69/Day69.java
@@ -0,0 +1,18 @@
+package day69;
+
+public class Day69 {
+
+    public static void main(String[] args) {
+        String [] Biodata = {
+            "Nama : Erick surya pratama",
+            "Nim : D0224342",
+            "angkatan : 2024"
+        };
+        System.out.println("Data diri");
+        for (String data : Biodata) {
+            System.out.println(data);
+        }
+            
+    }
+    
+}
