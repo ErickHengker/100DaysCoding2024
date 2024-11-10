0954233a59fd059d489d2851b75a96cd755562a2
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-11 00:04:31 +0800

    Create day70.java

---
+++ day70.java
@@ -0,0 +1,10 @@
+public class Day70 {
+
+    public static void halo(String nama) {
+        System.out.println("Nama saya : " + nama + ", Informatika A");
+    }
+
+    public static void main(String[] args) {
+        halo("Erick");
+    }
+}
