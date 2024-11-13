60063dbd24921b4e941ea3263b16b8f256ab03d2
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-13 21:50:45 +0800

    Create day 73.java

---
+++ day 73.java
@@ -0,0 +1,11 @@
+public class Day73 {
+    public static void main(String[] args) {
+        String nama = "erick surya pratama";
+        
+        char a = nama.charAt(5);
+        char b = nama.charAt(0);
+        
+        System.out.println("Indeks ke 5 : " + a);
+        System.out.println("Indeks ke 0 : " + b);
+    }
+    }
