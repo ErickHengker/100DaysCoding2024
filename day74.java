9b448c2d6e0ac9daac0917daa4da9ce130cd5c67
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-15 00:13:10 +0800

    Create day74.java

---
+++ day74.java
@@ -0,0 +1,18 @@
+package pp;
+public class Main {
+   
+    public static void main(String[] args) {
+        String nama = "Erick surya pratama";
+        int length = nama.length();
+        
+        System.out.println("Jumlah character di variable nama: " + length);
+    }
+}
+       
+
+
+        
+            
+        
+
+    
