45c0996220bb03458ae827f8df566ac10e899de3
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-15 21:54:32 +0800

    Create day75.java

---
+++ day75.java
@@ -0,0 +1,27 @@
+package pp;
+import java.util.Scanner;
+public class Main {
+   
+    public static void main(String[] args) {
+        
+        Scanner masuk = new Scanner(System.in);
+
+  System.out.print("ERICK SURYA PRATAMA ");
+        String nama = masuk.nextLine();
+
+        String ubah = nama.toLowerCase();
+
+        System.out.println("Teks dalam huruf kecil: " + ubah);
+
+        masuk.close();
+
+    }
+}
+       
+
+
+        
+            
+        
+
+
