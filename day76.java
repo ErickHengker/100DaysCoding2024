45a3da0b0287a23a0ca07e267a87d39eca5d0c61
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-16 23:44:04 +0800

    Create day76.java

---
+++ day76.java
@@ -0,0 +1,25 @@
+package pp;
+import java.util.Scanner;
+public class Main {
+   
+    public static void main(String[] args) {
+        
+        String nama = "erick surya pratama";
+
+       
+        String ubah = nama.toUpperCase();
+
+        
+        System.out.println("Awal: " + nama);
+        System.out.println("Hasil: " + ubah);
+    
+    }
+}
+       
+
+
+        
+            
+        
+
+
