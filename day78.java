5f26475afb693e3af4e609178063b2d1a1c89ddc
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-18 23:38:38 +0800

    Create day78.java

---
+++ day78.java
@@ -0,0 +1,21 @@
+package pp;
+public class Main {
+   
+    public static void main(String[] args) {
+        
+		        String nama1 = "Erick";
+        		String nama2 ="ErIck";
+        		String nama3 ="ERICK";
+        		System.out.println(" nama1 equalsIgnoreCase nama2:"+nama1. equalsIgnoreCase (nama2));
+        		System.out.println("nama1 equalsIgnoreCase nama3 :" + nama1. equalsIgnoreCase (nama3));
+        
+        }
+}
+       
+
+
+        
+            
+        
+
+
