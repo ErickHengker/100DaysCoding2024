4a5e42f2863d73bfaff0b915b213eea2f2a1637b
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-19 20:59:43 +0800

    Create day79.java

---
+++ day79.java
@@ -0,0 +1,11 @@
+
+package day79;
+
+public class Day79 {
+    public static void main(String[] args) {
+        int tgl = 27;
+        String a = Integer.toString(tgl);
+        System.out.println(a);
+    }
+    }
+
