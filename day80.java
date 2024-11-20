8cdbb7ee4b863ddca9bac11837ef5632c3d21825
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-20 21:54:36 +0800

    Create day80.java

---
+++ day80.java
@@ -0,0 +1,13 @@
+
+package aaaaa;
+
+public class AAAAA {
+
+    public static void main(String[] args) {
+         String hlo = "   Hello, World!   ";
+         String trm = hlo.trim();
+         System.out.println("Awal : "+hlo +"");
+         System.out.println("Trim : "+trm +"");
+    }
+    
+}
