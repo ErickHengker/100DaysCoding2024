4d60f6e8262500d6fbfd42c5371f42559a3fba92
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-22 00:32:34 +0800

    Create day82.java

---
+++ day82.java
@@ -0,0 +1,16 @@
+package day69;
+import java.math.*;
+public class NewClass1 {
+    public static void main(String[] args) {
+        double a = 15.5;
+        double b = 5.5;
+        double c = 10.2;
+        
+        System.out.println("nilai math "+a+" = "+(Math.ceil(a)));
+        System.out.println("nilai math "+b+" = "+(Math.floor(b)));
+        System.out.println("nilai math "+c+" = "+(Math.round(c)));
+        
+        
+    }
+    
+}
