0f59d0ddab67706d7480f8a1e54f88b9a6087609
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-24 22:50:01 +0800

    Create Day84.java

---
+++ Day84.java
@@ -0,0 +1,18 @@
+package pp;
+public class Main {
+   
+    public static void main(String[] args) {
+    String[] nama = new String[3];  
+    nama[0] = "Erick";
+    nama[1] = "Surya";
+    nama[2] = "Pratama";
+       }
+}
+       
+
+
+        
+            
+        
+
+    
