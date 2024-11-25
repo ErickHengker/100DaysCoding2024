b85eafb69ef78f9e047308ba0988081e14826cae
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-25 22:15:03 +0800

    Create day85.java

---
+++ day85.java
@@ -0,0 +1,19 @@
+package pp;
+public class Main {
+   
+    public static void main(String[] args) {
+    
+    int[] angka = {1, 2, 3, 4, 5};
+    for (int i : angka) {
+    System.out.println(i);
+}
+       }
+}
+       
+
+
+        
+            
+        
+
+
