0580a7ec09bc186a6beb7ef4e55892f7b025aa47
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-26 23:20:46 +0800

    Create day86.java

---
+++ day86.java
@@ -0,0 +1,22 @@
+package pp;
+public class Main {
+   
+    public static void main(String[] args) {
+        
+        int[] angka = {10, 20, 30, 40, 50};
+        System.out.println("Menggunakan for loop:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Elemen ke-" + i + ": " + angka[i]);
+        }
+
+        
+          }
+}
+       
+
+
+        
+            
+        
+
+
