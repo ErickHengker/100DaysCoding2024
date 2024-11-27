1e95c457b86667f6f3fdcccfdc1982eeb0f74bb4
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-28 00:34:25 +0800

    Create Day87.java

---
+++ Day87.java
@@ -0,0 +1,28 @@
+package pp;
+public class Main {
+   
+    public static void main(String[] args) {
+        
+        
+        int[] angka = {1, 2, 3, 4, 5};
+
+        // Mengubah nilai pada indeks ke-2
+        angka[2] = 10;
+
+        
+        for (int num : angka) {
+            System.out.print(num + " ");
+        }
+        
+    
+ 
+          }
+}
+       
+
+
+        
+            
+        
+
+
