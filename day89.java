22cad6c878f175b60c7ef67e1a737048b1e32537
ErickHengker <nholdyrealscout10133@gmail.com>
2024-11-29 22:07:32 +0800

    Update day89.java

--- day89.java
+++ day89.java
@@ -1,17 +1,20 @@
-package soalday89;
-
+package soalday88;
 
 public class charat {
     public static void main(String[] args) {
-        int [] nilai = {85,90,78,88,92};
+        int[] nilai = {85, 90, 78, 88, 92};
+
         for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Nilai sebelum :" + ":" + nilai[i]);
+            System.out.println("Nilai sebelum: " + nilai[i]);
         }
         nilai[3] = 89;
         for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Nilai sesudah " + ":" +nilai[i]);
+            System.out.println("Nilai sesudah: " + nilai[i]);
         }
-        
+        int total = 0;
+        for (int i = 0; i < nilai.length; i++) {
+            total += nilai[i];
+        }
+        double rataRata = (double) total / nilai.length;        System.out.println("Rata-rata nilai: " + rataRata);
     }
-    
 }
